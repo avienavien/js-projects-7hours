a3717fce849cf5a8fdef4cd29e65d9884310d7ec
avienavien <239125961+avienavien@users.noreply.github.com>
2026-09-02 15:46:47 +0400

    added functional save button, saved clear button, and added log counter

--- passenger-counterproj/index.js
+++ passenger-counterproj/index.js
@@ -1,16 +1,34 @@
 //icnrement the count variable when the button is clicked
 // change count el variable in html to reflect the new count
 
+ let saveEl = document.getElementById("save-el")
+ let countEl = document.getElementById("count-el")
+ 
+ 
+ let count = 0
 
-let countEl = document.getElementById("count-el")
-let count = 0
+ function increment() {
+     count += 1
+     countEl.innerText = count 
+ }
 
-function increment() {
-    count = count + 1
-    countEl.innerText = count 
-}
+ function clearCount() {
+    saveEl.innerText = "Previous Entries: "
+ }
 
-function clearCount() {
+ function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    console.log(count)
     count = 0
-    countEl.innerText = count;
-}
+    countEl.innerText = count
+ }
+ 
+ 
+ 
+ 
+
+ 
+
+
+
