a2dc7ccde8ad0f66ac790fb7e20a13bc22f58271
avienavien <239125961+avienavien@users.noreply.github.com>
2026-09-02 20:26:48 +0400

    removed comments in index.js

--- passenger-counterproj/index.js
+++ passenger-counterproj/index.js
@@ -1,6 +1,3 @@
-//icnrement the count variable when the button is clicked
-// change count el variable in html to reflect the new count
-
  let saveEl = document.getElementById("save-el")
  let countEl = document.getElementById("count-el")
  
