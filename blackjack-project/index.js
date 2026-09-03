db666f07283eabfd238600fb8dcfd622f5c61b07
avienavien <239125961+avienavien@users.noreply.github.com>
2026-09-03 15:52:17 +0400

    added newcard and its functionalities, starting random card function

--- blackjack-project/index.js
+++ blackjack-project/index.js
@@ -1,23 +1,51 @@
-let firstCard = 10
-let secondCard = 11
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+
 let sum = firstCard + secondCard 
 
 let hasBlackjack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+function getRandomCard() {
+    let randomCard = Math.floor( Math.random()*11 )
+    return randomCard
+}
+
 function startGame() {
+    renderGame()
+}
+function renderGame() {  
+    cardsEl.textContent = "Cards: " //resets
+    
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
     message = "Do you want to draw a new card?"
-}   
-else if (sum === 21) {
+    }   
+    else if (sum === 21) {
     message = "Blackjack! Yohoo!"
     hasBlackjack = true
-}
-else {
+    }
+    else {
     message = "You are out of the game."
     isAlive = false
+    }
+    messageEl.textContent = message
 }
-console.log(message)
 
-}
+function newCard() {
+    thirdCard = getRandomCard()
+    sum += thirdCard
+    cards.push(thirdCard)
+    
+    renderGame()
+} 
